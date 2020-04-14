0bfe3e5db33fd34987e6eb1886ca41c52733fee5
root <krishna.krish759213@gmail.com>
2020-04-14 10:13:42 +0000

    Update

--- s3basher.sh
+++ s3basher.sh
@@ -1,11 +1,12 @@
 sep=("." "_" "-")
 for s in ${sep[*]}; do
 word=$2$s$1;
-r=$(curl $word.s3.amazonaws.com -s);
+r=$(curl "http://"$word".s3.amazonaws.com/asasdasdd" -s);
+#echo $r;
 if [[ $r == *"AccessDenied"* ]]; then
 echo "Access Denied: "$word".s3.amazonaws.com"
 fi;
-if [[ $r == *"List"* ]]; then
+if [[ $r == *"NoSuchKey"* ]]; then
 echo "Public Bucket: "$word".s3.amazonaws.com"
 fi;
 
